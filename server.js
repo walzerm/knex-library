c53bceaa3579c0f01b5bb0a93454e1b948db5051
Lissa Walzer <lissawalzer@gmail.com>
2016-01-01 12:33:02 -0800

    Added the authors table

--- server.js
+++ server.js
@@ -10,8 +10,20 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
-app.get('/', function(req, res) {
-    res.send('Hello... its me');
+app.get('/authors', function(req, res) {
+    knex('authors').then(function(authors) {
+        res.render('index', {authors: authors});
+    })
+})
+
+app.get('/authors/new', function(req, res) {
+    res.render('new');
+})
+
+app.post('/authors', function(req, res) {
+    knex('authors').insert({name: req.body.new}).then(function() {
+        res.redirect('/authors');
+    })
 })
 
 app.listen(3000, function() {
